67de08b402721201ba3c83336873a23bc3f68960
Jatin Kumar <post2jatin@gmail.com>
2022-05-18 23:34:54 +0530

    ticket details page added

--- FlightBookingAngularProject/FlightBooking/src/app/ticketdetails/ticketdetails.component.ts
+++ FlightBookingAngularProject/FlightBooking/src/app/ticketdetails/ticketdetails.component.ts
@@ -1,6 +1,9 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BookedDetails } from '../models/bookeddetails';
 import { BookedPassengerDetails } from '../models/bookedpassenger';
+import { BookingDetails } from '../models/bookingdetails';
+import { PassengerDetails } from '../models/passengerdetails';
 import { EventService } from '../services/event.service';
 import { FilterPanelService } from '../services/filterpanel';
 
@@ -10,11 +13,12 @@ import { FilterPanelService } from '../services/filterpanel';
   styleUrls: ['./ticketdetails.component.css']
 })
 export class TicketdetailsComponent implements OnInit {
-  passengerDetails:Array<BookedPassengerDetails>=new Array<BookedPassengerDetails>();
+  passengerDetails:Array<PassengerDetails>=new Array<PassengerDetails>();
   ticketDetails: BookedDetails=new BookedDetails();
+  BookingDetails: BookingDetails=new BookingDetails();
   @ViewChild('BookinghistoryComponent', {static : false}) filterPanel: any;  
   constructor(public filterPanelService: FilterPanelService,
-    private _eventService: EventService) {
+    private _eventService: EventService,public datepipe: DatePipe) {
     
   }
 
@@ -22,11 +26,19 @@ export class TicketdetailsComponent implements OnInit {
     this.ticketDetails=this.filterPanelService.BookedData;
     console.log(this.ticketDetails)
     this._eventService.GetBookedPassengerDetails(this.ticketDetails.bookingId).subscribe(res=>this.SuccessGet(res),res=>this.ErrorGet(res));
-    //bind to model ==BookingDetails
+  
   }
   SuccessGet(res:any){    
-    this.passengerDetails = res
-    console.log(this.passengerDetails)
+    this.passengerDetails = res   
+      //bind to model ==BookingDetails
+      this.BookingDetails.FlightNumber=this.ticketDetails.flightNumber;
+      this.BookingDetails.Pnr=this.ticketDetails.pnr;
+      this.BookingDetails.Source=this.ticketDetails.source;
+      this.BookingDetails.Destination=this.ticketDetails.destination;
+      this.BookingDetails.TotalSeat=this.ticketDetails.totalSeat;
+      this.BookingDetails.CreatedOn= this.ticketDetails.createdOn;
+      this.BookingDetails.PassengerLists=this.passengerDetails
+      console.log(this.BookingDetails);
   }
   ErrorGet(res:any){
     console.log(res);     
